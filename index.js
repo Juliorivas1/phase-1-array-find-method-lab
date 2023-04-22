968523d420e9a7306ee5ffe914e10eaa3eb31d83
Julio Rivas <rivasjulio@ymail.com>
2023-04-22 14:28:13 -0400

    Complete"

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    const newObj = 
+    record.find(record => record.result === 'W')
+    return !!newObj ? newObj.year
+    : undefined
+}
